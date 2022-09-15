69165f244cea55e932b5f5f3280731f4c1898d8c
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-15 22:53:43 +0300

    15.09.2022

---
+++ src/test/java/day06/C04_Assertion.java
@@ -0,0 +1,17 @@
+package day06;
+
+public class C04_Assertion {
+
+
+
+
+
+
+
+    //https://www.youtube.com adresine gidin
+    //Aşağıdaki adları kullanarak 3 test metodu oluşturun ve gerekli testleri yapin
+    //titleTest => Sayfa başlığının “YouTube” oldugunu test edin
+    //imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+    // Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+    // wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+}
