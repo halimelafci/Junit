a99ccf578a42654a4a3edca9f37f3fd123f3a254
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-17 19:48:50 +0300

    17.09.2022

--- src/test/java/day08_Alert/C03.java
+++ src/test/java/day08_Alert/C03.java
@@ -2,7 +2,10 @@ package day08_Alert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -13,14 +16,15 @@ public class C03 {
     WebDriver driver;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
+
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.close();
     }
 
@@ -34,4 +38,19 @@ public class C03 {
     password    : admin
     Basarili sekilde sayfaya girildigini dogrulayin
      */
+
+    @Test
+    public void test1() {
+        //https://the-internet.herokuapp.com/basic_auth sayfasina gidin
+        //    asagidaki yontem ve test datalarini kullanarak authentication’i yapin
+        //
+        //    Html komutu : https://username:password@URL
+        //    Username  : admin
+        //    password  : admin
+        //
+        //    Basarili sekilde sayfaya girildigini dogrulayin
+        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+        Assert.assertTrue(driver.findElement(By.xpath("//h3")).isDisplayed());
+
+    }
 }
