73002fd47bf5e92a7a95726516741db1ae99f77e
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-17 18:07:23 +0300

    17.09.2022

--- src/test/java/day08_Alert/C01_Alerts.java
+++ src/test/java/day08_Alert/C01_Alerts.java
@@ -3,6 +3,8 @@ package day08_Alert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -23,4 +25,27 @@ public class C01_Alerts {
     public void tearDown() {
         //driver.close();
     }
+
+     // https://the-internet.herokuapp.com/javascript_alerts adresine gidelim
+     // Click for JS Alert butonuna tıklayalım
+    // Tıkladıktan sonra çıkan uyarı mesajına (Alerte) tamam diyelim
+
+    @Test
+
+    public void test1() throws InterruptedException {
+        // https://the-internet.herokuapp.com/javascript_alerts adresine gidelim
+        // Click for JS Alert butonuna tıklayalım
+
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+
+        // Tıkladıktan sonra çıkan uyarı mesajına (Alerte) tamam diyelim
+        Thread.sleep(5000);
+        driver.switchTo().alert().accept();
+    }
+
+
+
+
+
 }
---
+++ src/test/java/day08_Alert/C02_Allerts.java
@@ -0,0 +1,24 @@
+package day08_Alert;
+
+public class C02_Allerts {
+
+
+
+
+
+
+
+
+
+
+     //Bir class olusturun: Alerts
+    //https://the-internet.herokuapp.com/javascript_alerts adresine gidin.
+    //Bir metod olusturun: acceptAlert
+    //1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının  “You successfully clicked an alert” oldugunu test edin.
+    //Bir metod olusturun: dismissAlert
+    //2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının
+    //“successfuly” icermedigini test edin.
+    //Bir metod olusturun: sendKeysAlert
+    //3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna
+    //tıklayın ve result mesajında isminizin görüntülendiğini doğrulayın.
+}
