250b8f06654b3cf30ddb48320cf2c8a91fecdc1e
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-17 19:45:45 +0300

    17.09.2022

--- src/test/java/day08_Alert/C03.java
+++ src/test/java/day08_Alert/C03.java
@@ -1,5 +1,26 @@
 package day08_Alert;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C03 {
 
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+    }
 }
