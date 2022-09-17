6a369936165c2a9c3aac2e12146d58b507934757
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-17 19:47:47 +0300

    17.09.2022

--- src/test/java/day08_Alert/C03.java
+++ src/test/java/day08_Alert/C03.java
@@ -23,4 +23,15 @@ public class C03 {
     public void tearDown(){
         driver.close();
     }
+
+
+     /*
+    Bir class olusturun : BasicAuthentication
+    https://the-internet.herokuapp.com/basic_auth sayfasina gidin
+    asagidaki yontem ve test datalarini kullanarak authentication’i yapin
+    Html komutu : https://username:password@URL
+    Username    : admin
+    password    : admin
+    Basarili sekilde sayfaya girildigini dogrulayin
+     */
 }
