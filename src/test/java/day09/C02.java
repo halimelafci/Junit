05fa9f92c9aa484c2a48c57ad48a86b0ca0537bb
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-19 22:03:52 +0300

    19.09.2022

---
+++ src/test/java/day09/C02.java
@@ -0,0 +1,17 @@
+package day09;
+
+public class C02 {
+
+
+
+
+    //1- Amazon Sayfasina girelim
+    //2- Url'nin amazon içerdiğini test edelim
+    //3- Yeni bir pencere açıp bestbuy anasayfasına gidelim.(https://www.bestbuy.com)
+    //4- title'in BestBuy içerdiğini test edelim
+    //5- İlk sayfaya dönüp sayfada java aratalım
+    //6- Arama sonuclarının java içerdiğini test edelim
+    //7- Yeniden bestbuy sayfasına gidelim
+    //8- Logonun görünürlüğünü test edelim
+    //9- Sayfaları Kapatalım
+}
