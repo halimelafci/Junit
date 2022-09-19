650dba381d011c80ec1dee565b996334802c286f
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-19 22:21:41 +0300

    19.09.2022

--- src/test/java/day09/C02.java
+++ src/test/java/day09/C02.java
@@ -1,5 +1,17 @@
 package day09;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C02 {
 
 
@@ -14,4 +26,48 @@ public class C02 {
     //7- Yeniden bestbuy sayfasına gidelim
     //8- Logonun görünürlüğünü test edelim
     //9- Sayfaları Kapatalım
+
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+    @After
+    public void tearDown(){
+       // driver.close();
+    }
+
+    @Test
+
+    public void test1(){
+        //1- Amazon Sayfasina girelim
+        //2- Url'nin amazon içerdiğini test edelim
+
+        driver.get("https://amazon.com");
+        String amazonWindowHandel=driver.getCurrentUrl();
+        String expectedUrl="amazon";
+        String actualUrl=driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedUrl));
+
+        //3- Yeni bir pencere açıp bestbuy anasayfasına gidelim.(https://www.bestbuy.com)
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.get("https://www.bestbuy.com");
+
+        //4- title'in (Best Buy) içerdiğini test edelim
+        String actualTitle=driver.getTitle();
+        String expectetTitle="Best Buy";
+        Assert.assertTrue(actualTitle.contains(expectetTitle));
+
+        //5- İlk sayfaya dönüp sayfada java aratalım
+        driver.switchTo().window(amazonWindowHandel);
+    }
+
+
+
+
+
 }
