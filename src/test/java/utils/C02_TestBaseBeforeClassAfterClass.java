a949858e3d757a05e462a21d9126298c86800b3e
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-20 21:16:59 +0300

    20.09.2022

---
+++ src/test/java/utils/C02_TestBaseBeforeClassAfterClass.java
@@ -0,0 +1,28 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C02_TestBaseBeforeClassAfterClass {
+
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        //driver.quit();
+    }
+}
