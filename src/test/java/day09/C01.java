ce48cb8f67668d8274f5f3d9eabe6cbd2f56c1c9
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-23 21:15:13 +0300

    23.09.2022

--- src/test/java/day09/C01.java
+++ src/test/java/day09/C01.java
@@ -57,17 +57,25 @@ public class C01 {
         public void test1() throws InterruptedException {
             //● https://the-internet.herokuapp.com/iframe adresine gidin.
             driver.get("https://the-internet.herokuapp.com/iframe");
+
+
             //       ○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve konsolda yazdirin.
             WebElement iframeYazisi = driver.findElement(By.xpath("//h3"));
             Assert.assertTrue(iframeYazisi.isDisplayed());
             System.out.println(iframeYazisi.getText());
+
+
             //       ○ Text Box’a “Merhaba Dunya!” yazin.
-            WebElement textBox = driver.findElement(By.id("mce_0_ifr")); // Frame'e geçiş yapmadan önce orayı id ile locate ederiz
+            WebElement textBox = driver.findElement(By.id("mce_0_ifr"));
+            // Frame'e geçiş yapmadan önce orayı id ile locate ederiz
             driver.switchTo().frame(textBox); // Locate ettiğimiz frame geçmek için bu methodu kullanırız
+
             WebElement textBoxFrame = driver.findElement(By.xpath("//p"));
             textBoxFrame.clear(); // Clear methodu text box'ın içini temizler
             Thread.sleep(2000);
             textBoxFrame.sendKeys("Merhaba Dunya!");
+
+
             //       ○ TextBox’in altinda bulunan “Elemental Selenium” linkini textinin gorunur oldugunu
             //         dogrulayin ve konsolda yazdirin
             driver.switchTo().defaultContent(); // Iframe'den çıkıp ana sayfaya geçmek için bu method kullanılır
---
+++ src/test/java/day13/C01_Cookie.java
@@ -0,0 +1,60 @@
+package day13;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Cookie;
+import utils.TestBaseBforeAfter;
+
+import java.util.Set;
+
+public class C01_Cookie extends TestBaseBforeAfter {
+    @Test
+    public void test1(){
+
+        //1-Amazon anasayfaya gidin
+        //-tum cookie’leri listeleyin
+        //3-Sayfadaki cookies sayisinin 5’den buyuk oldugunu test edin
+        //4-ismi i18n-prefs olan cookie degerinin USD oldugunu test edin
+        // 5-ismi “en sevdigim cookie” ve degeri “cikolatali” olan bir cookie  olusturun ve sayfaya ekleyin
+        //6-eklediginiz cookie’nin sayfaya eklendigini test edin
+        //7-ismi skin olan cookie’yi silin ve silindigini test edin
+        //8-tum cookie’leri silin ve silindigini test edin
+
+
+
+        //1-Amazon anasayfaya gidin
+        driver.get("https://amazon.com");
+
+        //-tum cookie’leri listeleyin
+        Set<Cookie> tumCookie=driver.manage().getCookies();
+        System.out.println(tumCookie);
+         int sayac=1;
+
+        for (Cookie each:tumCookie) {
+            System.out.println(sayac +". cookie : "+ each);
+            System.out.println(sayac +". name : "+ each.getName());
+            System.out.println(sayac + ". value : "+ each.getValue());
+            sayac++;
+
+            //3-Sayfadaki cookies sayisinin 5’den buyuk oldugunu test edin
+            int cookiesSayisi=tumCookie.size();
+            Assert.assertTrue(cookiesSayisi>5);
+
+            //4-ismi i18n-prefs olan cookie degerinin USD oldugunu test edin
+            for (Cookie each2:tumCookie) {
+                if (each2.getName().equals("i18n-prefs")){
+                    Assert.assertEquals("USD",each2.getValue());
+                }
+            }
+
+            // 5-ismi “en sevdigim cookie” ve degeri “cikolatali” olan bir cookie  olusturun ve sayfaya ekleyin
+
+
+
+        }
+
+
+
+
+    }
+}
