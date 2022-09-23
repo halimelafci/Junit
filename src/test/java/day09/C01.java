a9ac85b6a07ac2db6ab8136639ba37bef89e79bb
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-23 16:48:54 +0300

    23.09.2022

--- src/test/java/day09/C01.java
+++ src/test/java/day09/C01.java
@@ -41,20 +41,40 @@ public class C01 {
     }
 
 
-    @Test
-    public void test1(){
-        // ● https://the-internet.herokuapp.com/iframe adresine gidin.
-        driver.get("https://the-internet.herokuapp.com/iframe");
 
-        //○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve konsolda yazdirin.
-        WebElement iframeYazisi=driver.findElement(By.xpath("//h3"));
-        Assert.assertTrue(iframeYazisi.isDisplayed());
-        System.out.println(iframeYazisi.getText());
+           /*
+    ● Bir class olusturun: IframeTest
+    ● https://the-internet.herokuapp.com/iframe adresine gidin.
+    ● Bir metod olusturun: iframeTest
+       ○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve konsolda yazdirin.
+       ○ Text Box’a “Merhaba Dunya!” yazin.
+       ○ TextBox’in altinda bulunan “Elemental Selenium” linkini textinin gorunur oldugunu
+         dogrulayin ve konsolda yazdirin
+     */
 
-        //○ Text Box’a “Merhaba Dunya!” yazin.
 
-
-    }
+        @Test
+        public void test1() throws InterruptedException {
+            //● https://the-internet.herokuapp.com/iframe adresine gidin.
+            driver.get("https://the-internet.herokuapp.com/iframe");
+            //       ○ “An IFrame containing….” textinin erisilebilir oldugunu test edin ve konsolda yazdirin.
+            WebElement iframeYazisi = driver.findElement(By.xpath("//h3"));
+            Assert.assertTrue(iframeYazisi.isDisplayed());
+            System.out.println(iframeYazisi.getText());
+            //       ○ Text Box’a “Merhaba Dunya!” yazin.
+            WebElement textBox = driver.findElement(By.id("mce_0_ifr")); // Frame'e geçiş yapmadan önce orayı id ile locate ederiz
+            driver.switchTo().frame(textBox); // Locate ettiğimiz frame geçmek için bu methodu kullanırız
+            WebElement textBoxFrame = driver.findElement(By.xpath("//p"));
+            textBoxFrame.clear(); // Clear methodu text box'ın içini temizler
+            Thread.sleep(2000);
+            textBoxFrame.sendKeys("Merhaba Dunya!");
+            //       ○ TextBox’in altinda bulunan “Elemental Selenium” linkini textinin gorunur oldugunu
+            //         dogrulayin ve konsolda yazdirin
+            driver.switchTo().defaultContent(); // Iframe'den çıkıp ana sayfaya geçmek için bu method kullanılır
+            WebElement elemantal = driver.findElement(By.xpath("//*[text()='Elemental Selenium']"));
+            Assert.assertTrue(elemantal.isDisplayed());
+            System.out.println(elemantal.getText());
+        }
 
 
 
