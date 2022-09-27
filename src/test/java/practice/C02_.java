09c237bcb93804ad568ed183d3f10bc339c821d1
Halime Lafcı <halimebabacan9@gmail.com>
2022-09-27 20:34:24 +0300

    27.09.2022

--- src/test/java/practice/C02_.java
+++ src/test/java/practice/C02_.java
@@ -1,8 +1,21 @@
 package practice;
 
+import org.junit.Assert;
+import org.junit.Test;
 import utils.TestBaseBforeAfter;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class C02_ extends TestBaseBforeAfter {
 
+    //Verify that we have pom.xml file in our project => please try in 4 min s
 
+    @Test
+    public void test01(){
+        String dosyaYolu="pom.xml";
+
+        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+    }
 }
